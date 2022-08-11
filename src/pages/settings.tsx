2ff702e87b347f12909fd8412c1c07b66ed9a88f
Quorin <xquoris@gmail.com>
2022-08-11 19:31:08 +0200

    feat(settings): layout

---
+++ src/pages/settings.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+
+const Settings = () => {
+  return (
+    <div className="flex flex-col gap-6">
+      <h2 className="text-3xl text-zinc-200 mb-10 font-semibold self-center">
+        Settings
+      </h2>
+
+      <div className="flex flex-col gap-6 justify-center">
+        <Link href={''}>
+          <button className="w-full md:w-1/2 bg-zinc-700 hover:bg-zinc-300 text-zinc-300 hover:text-zinc-700 transition-colors mx-auto px-10 py-3 rounded-lg">
+            Change Password
+          </button>
+        </Link>
+        <Link href={''}>
+          <button className="w-full md:w-1/2 bg-zinc-700 hover:bg-zinc-300 text-zinc-300 hover:text-zinc-700 transition-colors mx-auto px-10 py-3 rounded-lg">
+            Change Email
+          </button>
+        </Link>
+        <Link href={''}>
+          <button className="w-full md:w-1/2 bg-zinc-700 hover:bg-zinc-300 text-zinc-300 hover:text-zinc-700 transition-colors mx-auto px-10 py-3 rounded-lg">
+            Change Name
+          </button>
+        </Link>
+        <Link href={''}>
+          <button className="w-full md:w-1/2 bg-zinc-700 hover:bg-red-300 text-red-300 hover:text-zinc-700 transition-colors mx-auto px-10 py-3 rounded-lg">
+            Remove Account & Data
+          </button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Settings
