8416c02c615a44705a0da089a34cd8f5e8d93baa
Quorin <xquoris@gmail.com>
2022-08-12 01:21:15 +0200

    refactor(modal): background color & opacity change

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -40,7 +40,7 @@ const Modal = ({
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          <div className="fixed inset-0 bg-black bg-opacity-60 transition-opacity" />
         </Transition.Child>
 
         <div className="fixed z-10 inset-0 overflow-y-auto">
