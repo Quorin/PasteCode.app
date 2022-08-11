9302235f8b7dfc59e60a221f9df0fdf4ecf37062
Quorin <xquoris@gmail.com>
2022-08-12 00:37:50 +0200

    feat(settings): change password

--- src/server/router/settings-router.ts
+++ src/server/router/settings-router.ts
@@ -4,8 +4,61 @@ import * as trpc from '@trpc/server'
 import { generateRandomString } from '../../utils/random'
 import { sendConfirmationEmail } from '../../utils/email'
 import dayjs from 'dayjs'
+import * as argon2 from 'argon2'
 
 export const settingsRouter = createProtectedRouter()
+  .mutation('changePassword', {
+    input: z
+      .object({
+        currentPassword: z.string(),
+        password: z
+          .string()
+          .regex(
+            new RegExp(
+              '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})',
+            ),
+            'Password must contain at least one lowercase letter, one uppercase letter, one number and one special character',
+          ),
+        confirmPassword: z.string(),
+      })
+      .refine((data) => data.password === data.confirmPassword, {
+        message: 'Passwords do not match',
+        path: ['confirmPassword'],
+      }),
+    async resolve({ input, ctx }) {
+      const user = await ctx.prisma.user.findFirst({
+        where: { id: ctx.session.user.id! },
+        select: { password: true },
+      })
+
+      if (!user) {
+        throw new trpc.TRPCError({
+          code: 'UNAUTHORIZED',
+          message: 'User not found',
+        })
+      }
+
+      if (!(await argon2.verify(user.password, input.currentPassword))) {
+        throw new trpc.TRPCError({
+          code: 'BAD_REQUEST',
+          cause: new ZodError([
+            {
+              path: ['currentPassword'],
+              message: 'Current password is incorrect',
+              code: 'custom',
+            },
+          ]),
+        })
+      }
+
+      await ctx.prisma.user.update({
+        where: { id: ctx.session.user.id! },
+        data: {
+          password: await argon2.hash(input.password),
+        },
+      })
+    },
+  })
   .mutation('changeEmail', {
     input: z
       .object({
