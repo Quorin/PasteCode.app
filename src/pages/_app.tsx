25fb56ab4d4e2c250e1beafe30b4218c0280d88e
Quorin <xquoris@gmail.com>
2022-08-22 18:14:46 +0200

    feat(app): env for app url

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,6 +21,11 @@ export const getBaseUrl = () => {
   if (typeof window !== 'undefined') {
     return ''
   }
+
+  if (process.env.NODE_ENV === 'production' && process.env.APP_URL) {
+    return `https://${process.env.APP_URL}`
+  }
+
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}` // SSR should use vercel url
 
   return `http://localhost:${process.env.PORT ?? 3000}` // dev SSR should use localhost
