9ffc322c1b075f76fc422c8e799cc1b92a301914
Quorin <xquoris@gmail.com>
2023-04-14 20:58:53 +0200

    refactor(navbar): better auth behaviour

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -34,7 +34,7 @@ const Navbar = () => {
   const [menuCollapsed, setMenuCollapsed] = useState(true)
   const menuRef = useRef<HTMLDivElement>(null)
   const router = useRouter()
-  const { user, isLoggedIn } = useAuth()
+  const { user, isLoggedIn, isLoading } = useAuth()
 
   const handleCollapse = () => {
     setMenuCollapsed(!menuCollapsed)
@@ -81,23 +81,30 @@ const Navbar = () => {
           id="navbar-default"
         >
           <ul className="flex flex-col p-4 mt-4 rounded-lg border md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 bg-zinc-800 md:bg-zinc-900 border-zinc-700 text-center">
-            {isLoggedIn
-              ? authorizedPaths(user!.name).map(({ path, name }) => (
-                  <li
-                    key={path}
-                    className={link({ active: router.pathname === path })}
-                  >
-                    <Link href={path}>{name}</Link>
-                  </li>
-                ))
-              : unauthorizedPaths.map(({ path, name }) => (
-                  <li
-                    key={path}
-                    className={link({ active: router.pathname === path })}
-                  >
-                    <Link href={path}>{name}</Link>
-                  </li>
-                ))}
+            {isLoading && (
+              <div className="cursor-none select-none py-2.5"></div>
+            )}
+            {!isLoading && (
+              <>
+                {isLoggedIn
+                  ? authorizedPaths(user!.name).map(({ path, name }) => (
+                      <li
+                        key={path}
+                        className={link({ active: router.pathname === path })}
+                      >
+                        <Link href={path}>{name}</Link>
+                      </li>
+                    ))
+                  : unauthorizedPaths.map(({ path, name }) => (
+                      <li
+                        key={path}
+                        className={link({ active: router.pathname === path })}
+                      >
+                        <Link href={path}>{name}</Link>
+                      </li>
+                    ))}
+              </>
+            )}
           </ul>
         </div>
       </div>
