65e1b6cd16e475a7f3c0de6d5ff8fa4f516ce8dd
Quorin <xquoris@gmail.com>
2023-10-23 14:50:22 +0200

    feat(db): prisma bump up

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,15 +1,21 @@
 // src/server/db/client.ts
 import { PrismaClient } from '@prisma/client'
 import { env } from '../../env/server.mjs'
+import { connect } from '@planetscale/database'
+import { PrismaPlanetScale } from '@prisma/adapter-planetscale'
 
 declare global {
   var prisma: PrismaClient | undefined
 }
 
+const connection = connect({ url: env.DATABASE_URL })
+const adapter = new PrismaPlanetScale(connection)
+
 export const prisma =
   global.prisma ||
   new PrismaClient({
     log: env.NODE_ENV === 'production' ? [] : ['query'],
+    adapter,
   })
 
 if (env.NODE_ENV !== 'production') {
