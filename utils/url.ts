2581757768d91930e0d1dcf9ed267771dbc1ac11
Quorin <xquoris@gmail.com>
2024-01-06 19:35:19 +0100

    fix: base url

--- utils/url.ts
+++ utils/url.ts
@@ -1,5 +1,7 @@
 export const getBaseUrl = () => {
-  return process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : 'http://localhost:3000'
+  return process.env.APP_URL
+    ? `https://${process.env.APP_URL}`
+    : process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : 'http://localhost:3000'
 }
