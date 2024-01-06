5cb4691783b95539dc0312bef826565b8399e346
Quorin <xquoris@gmail.com>
2024-01-06 19:59:37 +0100

    refactor(random): cryptographically random string

--- utils/random.ts
+++ utils/random.ts
@@ -1,8 +1,11 @@
 const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
-export const generateRandomString = (length: number): string =>
-  Array(length)
-    .join()
-    .split(',')
-    .map(() => chars.charAt(Math.floor(Math.random() * chars.length)))
+export const generateRandomString = (length: number): string => {
+  const array = new Uint8Array(length)
+
+  crypto.getRandomValues(array)
+
+  return Array.from(array)
+    .map((num) => chars[num % chars.length])
     .join('')
+}
