f42bdd7b663e197ca53abf53e23d5867385a2a3a
Quorin <xquoris@gmail.com>
2024-05-10 22:46:51 +0200

    chore: package versions bump up

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,9 +2,9 @@ import type { Config } from 'drizzle-kit'
 
 export default {
   dbCredentials: {
-    connectionString: process.env.POSTGRES_URL!,
+    url: process.env.POSTGRES_URL!,
   },
-  driver: 'pg',
+  dialect: 'postgresql',
   out: './drizzle',
   schema: './db/schema.ts',
 } satisfies Config
